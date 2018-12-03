3aad247e3ef4d5a51aabc1d4e5eced50b16b56df
Vilgot Lilja <lilja.vilgot@gmail.com>
2018-12-03 12:07:47 +0100

    objects
    tried the indentaion of object and started working on functions

--- Assignment_A2/Assignment_A2/main.cpp
+++ Assignment_A2/Assignment_A2/main.cpp
@@ -1,11 +1,16 @@
 #include "Shape.h"
 #include "Polygon.h"
 #include <iostream>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+
+	Polygon* foo = new Polygon();
+	foo = new Polygon[6];
 
 
 	return 0;
